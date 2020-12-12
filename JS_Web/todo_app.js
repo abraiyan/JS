ddf425c9f9190f9a73ba1e2b9dd351b0e576a6fb
abraiyan <abraiyan20@gmail.com>
2020-12-12 18:46:12 +0600

    FC

--- JS_Web/todo_app.js
+++ JS_Web/todo_app.js
@@ -1,51 +1,8 @@
-let todoArray = []
+let todoArray = getSavedTodos()
 
 let filterObject = {
     searchText: '',
     hideCompleted: false
-}
-
-const todosJSON = localStorage.getItem('todos')
-
-if(todosJSON !== null) {
-    todoArray = JSON.parse(todosJSON)
-}
-
-function addItem (todoTitle) {
-    todoArray.push({
-        title: todoTitle,
-        isCompleted: false
-    })
-    localStorage.setItem('todos', JSON.stringify(todoArray))
-    countLeftTodos()
-    renderTodos(filterObject)
-}
-
-function countLeftTodos () {
-    let counter = 0
-    todoArray.forEach(function (item) {
-        if(!item.isCompleted) counter++
-    })
-    document.getElementById('left_todos').textContent = `You have ${counter} todos left to do`
-}
-
-function clearTodoDiv () {
-    document.getElementById('todo_section').innerHTML = ''
-}
-
-function renderTodos (filterObject) {
-    const filteredTodos = todoArray.filter(function (item) {
-        const isIncluded = item.title.toLowerCase().includes(filterObject.searchText.toLowerCase())
-        const showUncompleted = !filterObject.hideCompleted || !item.isCompleted
-        return isIncluded && showUncompleted
-    })
-
-    clearTodoDiv()    
-    filteredTodos.forEach(function (item) {
-        const p = document.createElement('p')
-        p.textContent = item.title
-        document.getElementById('todo_section').appendChild(p)
-    })
 }
 
 renderTodos(filterObject)
---
+++ JS_Web/todo_functions.js
@@ -0,0 +1,50 @@
+function getSavedTodos () {
+    const todosJSON = localStorage.getItem('todos')
+
+    if(todosJSON !== null) {
+        return JSON.parse(todosJSON)
+    } else {
+        return []
+    }
+}
+
+function addItem (todoTitle) {
+    todoArray.push({
+        title: todoTitle,
+        isCompleted: false
+    })
+    localStorage.setItem('todos', JSON.stringify(todoArray))
+    countLeftTodos()
+    renderTodos(filterObject)
+}
+
+function countLeftTodos () {
+    let counter = 0
+    todoArray.forEach(function (item) {
+        if(!item.isCompleted) counter++
+    })
+    document.getElementById('left_todos').textContent = `You have ${counter} todos left to do`
+}
+
+function clearTodoDiv () {
+    document.getElementById('todo_section').innerHTML = ''
+}
+
+function renderTodos (filterObject) {
+    const filteredTodos = todoArray.filter(function (item) {
+        const isIncluded = item.title.toLowerCase().includes(filterObject.searchText.toLowerCase())
+        const showUncompleted = !filterObject.hideCompleted || !item.isCompleted
+        return isIncluded && showUncompleted
+    })
+
+    clearTodoDiv()    
+    filteredTodos.forEach(function (item) {
+        appendChildAtTodoDiv()
+    })
+}
+
+function appendChildAtTodoDiv (item) {
+    const p = document.createElement('p')
+    p.textContent = item.title
+    document.getElementById('todo_section').appendChild(p)
+}
