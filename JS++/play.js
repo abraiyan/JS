ebf862645b996c6ef3aefe390aa3938e9f66f425
abraiyan <abraiyan20@gmail.com>
2020-12-19 07:22:54 +0600

    FC

--- JS++/play.js
+++ JS++/play.js
@@ -1,12 +1,22 @@
-const calculateTip = (amount, tipPercentage) => {
-  if (typeof amount !== 'number' || typeof tipPercentage !== 'number') {
-    throw Error('Input should be a number')
+class Student {
+  constructor(firstName, lastName, age) {
+    this.firstName = firstName
+    this.lastName = lastName
+    this.age = age
   }
-  return amount * (tipPercentage / 100)
+  getBio() {
+    return `${this.firstName} is ${this.age}`
+  }
+  setName(fullName) {
+    const names = fullName.split(' ')
+    this.firstName = names[0]
+    this.lastName = names[1]
+  }
 }
 
-try {
-  console.log(`calculated tip : ${calculateTip(200, 20)}`)
-} catch (e) {
-  console.log(e.message)
-}
+const studentOne = new Student('AB', 'Raiyan', 21)
+const studentTwo = new Student('John', 'Doe', 23)
+
+console.log(studentOne.getBio())
+studentOne.setName('Nasrat Yousha')
+console.log(studentOne.getBio())
