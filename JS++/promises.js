b431d362855b4ca5f1dfcee94130637a6abe4fa3
abraiyan <abraiyan20@gmail.com>
2021-01-03 18:04:22 +0600

    .

--- JS++/promises.js
+++ JS++/promises.js
@@ -1,21 +1,18 @@
-const weatherByCityName = (cityName) => {
-  return fetch(
+const weatherByCityName = async (cityName) => {
+  const response = await fetch(
     `http://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=b10461359bf7b6e4b19839f0fa739c6b&units=metric`,
     {}
   )
-    .then((response) => {
-      if (response.status === 200) {
-        return response.json()
-      } else {
-        throw new Error('GG hobena')
-      }
-    })
-    .then((data) => data.main.temp)
+  if (response.status === 200) {
+    return response.json()
+  } else {
+    throw new Error('GG hobena')
+  }
 }
 
 weatherByCityName(prompt('Enter City Name'))
   .then((data) => {
-    console.log(data)
+    console.log(data.main.temp)
   })
   .catch((error) => {
     console.log(`Lol ${error}`)
