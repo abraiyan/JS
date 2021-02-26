5aeebdc3b36d48e0f4ed4090613225f93a28cb56
abraiyan <abraiyan20@gmail.com>
2021-02-26 17:53:54 +0600

    todo app complete

--- AnotherTodo/app.js
+++ AnotherTodo/app.js
@@ -6,10 +6,15 @@ class ToDo {
   }
 }
 
-const todoArray = []
-
 class UI {
   static displayToDo() {
+    const todoArray = Store.getTodo()
+
+    const todoLeftCounter = document.getElementById('left-todo')
+    todoLeftCounter.textContent = `You have ${
+      Store.countLeftTodos().counter
+    } task${Store.countLeftTodos().anExtraS} to do`
+
     const todoList = document.getElementById('todo-list')
     document.getElementById('todo-list').innerHTML = ``
 
@@ -52,18 +57,69 @@ class UI {
   }
 
   static deleteTodo(id) {
-    const index = todoArray.findIndex((todo) => {
-      return todo.id === id
-    })
-    console.log(index)
-    todoArray.splice(index, 1)
+    Store.deleteTodo(id)
     this.displayToDo()
   }
 
   static addTodo(todoTitle) {
     const todoItem = new ToDo(uuidv4(), todoTitle, false)
-    todoArray.push(todoItem)
+    Store.addTodo(todoItem)
     this.displayToDo()
+  }
+
+  static showAlert(message, className) {
+    const middleSection = document.querySelector('.middle-section')
+
+    const alertDiv = document.createElement('div')
+    alertDiv.className = `alert ${className}`
+    alertDiv.appendChild(document.createTextNode(message))
+
+    middleSection.appendChild(alertDiv)
+
+    setTimeout(() => {
+      middleSection.removeChild(alertDiv)
+    }, 1500)
+  }
+}
+
+class Store {
+  static getTodo() {
+    let todos
+    if (localStorage.getItem('todos') === null) {
+      todos = []
+    } else {
+      todos = JSON.parse(localStorage.getItem('todos'))
+    }
+    return todos
+  }
+
+  static addTodo(todoItem) {
+    let todos = this.getTodo()
+    todos.push(todoItem)
+    localStorage.setItem('todos', JSON.stringify(todos))
+    UI.showAlert('TODO Added Successfully', 'message')
+  }
+
+  static deleteTodo(todoIndex) {
+    let todos = this.getTodo()
+    const index = todos.findIndex((todo) => {
+      return todo.id === todoIndex
+    })
+    todos.splice(index, 1)
+    localStorage.setItem('todos', JSON.stringify(todos))
+    UI.showAlert('TODO Deleted Successfully', 'alert')
+  }
+
+  static countLeftTodos() {
+    let counter = 0
+    let anExtraS = ''
+    this.getTodo().map((todo) => {
+      if (!todo.status) {
+        counter++
+      }
+    })
+    if (counter > 1) anExtraS = 's'
+    return { counter: counter, anExtraS: anExtraS }
   }
 }
 
